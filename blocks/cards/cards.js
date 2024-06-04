ce45cc978c1498541e3d44bed86d8ecac9a4f16d
Harika Thota <50997+hthota@users.noreply.git.corp.adobe.com>
2024-06-04 19:54:20 +0530

    placeholders

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,14 +1,22 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { fetchPlaceholders } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
+  const placeholders = await fetchPlaceholders();
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else {
+        div.className = 'cards-card-body';
+        const placeholderElement = document.createElement('p');
+        placeholderElement.textContent = placeholders.clickHereForMore;
+        div.appendChild(placeholderElement);
+      }
     });
     ul.append(li);
   });
